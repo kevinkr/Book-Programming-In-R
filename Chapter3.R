317be82fecf1da98d2574db47ae40c4f5fd0d5a9
kevinkr <kevinkr@yahoo.com>
2016-09-07 21:18:20 -0500

    almost done

--- Chapter3.R
+++ Chapter3.R
@@ -32,6 +32,11 @@ confint <- function(x, alpha = .05) {
   cat("Upper Limit:", upper, "\n")
 }
 
+#use default alpha level
+confint(Age)
+
+#change alpha level
+confint(Age, 0.10)
 
 ##########
 #VECTORIZED OPERATIONS
@@ -54,11 +59,81 @@ y
 
 #ifelse
 x <- -5:5
-sqrt(ifelse(x>0, x, NA))
 
+#generates NaNs
 ifelse(x>=0, sqrt(x), NA)
+
+#avoids geenrationg NaNs
+sqrt(ifelse(x>0, x, NA))
 
 #More powerful functions
 primes <- read.csv("./Data/primes1000.csv", header=T)
 head(primes)
 primes$n <- NULL
+summary(primes)
+str(primes)
+
+#primality function to test if number is prime
+primality <- function (x) {
+  stopifnot(x>=2)
+  limit <- trunc(sqrt(x) + 1) #find sq rt of x adn round up by 1
+  testvec <- 2:limit
+  results <- x %% testvec #dvide vector by list of primes
+  check <- any(results == 0)
+  outcome <- "Yes."
+  if (check == TRUE) outcome <- "No."
+  if (x == 2)  outcome <- "Yes."
+  cat("Is ", x, " prime? ", outcome, "\n" )
+}
+
+#########
+#Makng Functions more useful
+##########
+#
+#quartile function
+qt
+?qt
+
+t.test
+?t.test
+
+#load weights data
+load("./Data/weights.rda")
+weights
+confint(weights, alpha = .10)
+
+t.test(weights, mu = 191, conf.level = .90)
+
+confint
+
+#new conf int with flow control to determine if there is a one-tailed or two-tailed test
+confint.1 <- function(x, alpha = .05, two.tailed = TRUE) {
+  cat("Mean: ", mean(x), "\n")
+  df <- length(x) - 1
+  stderr <- sd(x)/sqrt(length(x))
+  cat("Standard error of the mean: ", stderr, "\n")
+  conflevel <- 1 - alpha/2
+  if (two.tailed == FALSE) {
+    conflevel <- 1 - alpha
+  }
+  tcrit <- qt(conflevel, df)
+  margin <- stderr * tcrit
+  LL <- mean(x) - margin
+  UL <- mean(x) + margin
+  if (two.tailed == FALSE) {
+    cat("You are doing a one-tailed test.","\n")
+    cat("If your test is left-tailed,","\n")
+    cat("the lower bound is negative infinity.","\n")
+    cat("If your test is right-tailed,","\n")
+    cat("the upper bound is positive infinity.","\n")
+    cat("Either add the margin",margin,"to or subract it from","\n")
+    cat("the sample mean as appropriate.","\n")
+    cat("For a left-tailed test, the upper bound is",LL,"\n")
+    cat("For a right-tailed test, the lower bound is",UL,"\n")
+  }
+  cat("upper bound:",LL,"\n")
+  cat("lower bound:",UL,"\n")
+}
+
+confint.1(weights)
+
